e21f655b59aa6f98f98d878a6f15f0446c4a3257
Lewis Clements <lewisclements15@icloud.com>
2024-04-08 22:29:27 +0000

    Add clubs and distance range for each club

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Clubs
+driver_distance = [range(220,260, 5)]
+five_distance = [range(155, 185, 5)]
+seven_distance = [range(140, 160, 5)]
+nine_distance = [range(115, 135,5)]
+pitching_wedge_distance = [range(60, 110, 5)]
+lob_wedge_distance = [range(20, 55, 5)]
+putts = [1,2,3] 
