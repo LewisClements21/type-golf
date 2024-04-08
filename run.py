2a3313c9c45a0addaae69323e480367a374c7427
Lewis Clements <lewisclements15@icloud.com>
2024-04-08 22:30:34 +0000

    Add function to randomise distance  in range setfor each club

--- run.py
+++ run.py
@@ -6,3 +6,32 @@ nine_distance = [range(115, 135,5)]
 pitching_wedge_distance = [range(60, 110, 5)]
 lob_wedge_distance = [range(20, 55, 5)]
 putts = [1,2,3] 
+
+#Define functions for hitting clubs
+def hit_driver():
+    club_distance = random.choice(driver_distance)
+    return club_distance
+
+def hit_five_iron():
+    club_distance = random.choice(five_distance)
+    return club_distance
+
+def hit_seven_iron():
+    club_distance = random.choice(seven_distance)
+    return club_distance
+
+def hit_nine_iron():
+    club_distance = random.choice(nine_distance)
+    return club_distance
+
+def hit_pitching_wedge():
+    club_distance = random.choice(pitching_wedge_distance)
+    return club_distance
+
+def hit_lob_wedge():
+    club_distance = random.choice(lob_wedge_distance)
+    return club_distance
+
+def putt():
+    putt = random.choice(putts)
+    return putt
