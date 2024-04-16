0764ab425765895509959b4b24b59d15abaed4a6
Lewis Clements <lewisclements15@icloud.com>
2024-04-16 20:48:47 +0000

    Add loop which lets players choose clubs while they are over 20 yards away otherwise a random number from 1-3 is selected for the amount of putts taken. Add score info and exit prompt.

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import random
 
 #Clubs
-driver_distance = [range(220,260, 5)]
-five_distance = [range(155, 185, 5)]
-seven_distance = [range(140, 160, 5)]
-nine_distance = [range(115, 135,5)]
-pitching_wedge_distance = [range(60, 110, 5)]
-lob_wedge_distance = [range(20, 55, 5)]
-putts = [1,2,3] 
+driver_distance = list(range(220,260, 5))
+five_distance = list(range(155, 185, 5))
+seven_distance = list(range(140, 160, 5))
+nine_distance = list(range(115, 135,5))
+pitching_wedge_distance = list(range(60, 110, 5))
+lob_wedge_distance = list(range(15, 55, 5))
+putter = [1,2,3] 
 
 #Define functions for hitting clubs
 def hit_driver():
@@ -35,14 +35,14 @@ def hit_lob_wedge():
     return club_distance
 
 def putt():
-    putt = random.choice(putts)
+    putt = random.choice(putter)
     return putt
 
 #Course Setup
-full_range = [range(130,520,5)]
-par3 = [range(130,250,5)]
-par4 = [range(250,440,5)]
-par5 = [range(440,500,5)]
+full_range = list(range(130,520,5))
+par3 = list(range(130,250,5))
+par4 = list(range(250,440,5))
+par5 = list(range(440,500,5))
 par3_4 = par3 + par4
 par3_5 = par3 + par5
 par4_5 = par4 + par5
@@ -72,7 +72,7 @@ else:
     print("Good Luck!")
     print()
 
-club_choices = "Here is your bag: Driver, 5 Iron, 7 Iron, 9 Iron, Pitching Wedge, Lob Wedge, Putter."
+club_choices = "Here is your bag: Driver, 5 Iron, 7 Iron, 9 Iron, Pitching Wedge, Lob Wedge."
 
 print(club_choices)
 print()
@@ -168,3 +168,57 @@ for x in range(1,10):
             print("You have " + str(distance_remaining) + " yards left.")
             print()
             hole_shots += 1
+    
+    
+    #Finish the hole by putting
+    if distance_remaining < 20:
+        if distance_remaining == 0:
+            putts = 0
+        else:
+            putts = putt()
+            hole_strokes = hole_shots + putts     
+            hole_to_par = hole_strokes - par
+            total_strokes += hole_strokes
+            total_to_par += hole_to_par
+
+        #Show player hole/round scoring info
+        if putts == 0:
+            print("You're in the hole! You're in for " + str(hole_strokes) + " strokes.")
+        else:
+            print("You're on the green! After " + str(putts) + " putt(s), you're in for " + str(hole_strokes) + " strokes.")
+        if hole_to_par > 0:
+            print("Your score for the hole is +" + str(hole_to_par) + " to par.")
+        elif hole_to_par == 0:
+            print("Your score for the hole is even to par.")
+        else:    
+            print("Your score for the hole is " + str(hole_to_par) + " to par.")
+        print()
+        print("You've hit a total of " + str(total_strokes) + " strokes so far.") 
+        if total_to_par > 0:
+            print("Your score for the round is +" + str(total_to_par) + " to par.")
+        elif total_to_par == 0:
+            print("Your score for the round is even to par.")    
+        else:    
+            print("Your score for the round is " + str(total_to_par) + " to par.")       
+        print()
+        print()
+        print()  
+
+#Final score messages & exit prompt      
+print("Congratulations on finishing your 9-hole round!")
+print("Your stroke total is " + str(total_strokes) + ".")
+if total_to_par > 0:
+    print("Your score for the day is +" + str(total_to_par) + " to par.")
+elif total_to_par == 0:
+    print("Your score for the day is even to par.")    
+else:
+    print("Your score for the day is " + str(total_to_par) + " to par.")
+print()
+print()
+print("THANKS FOR PLAYING!")
+print()
+print()
+
+leave = input("type 'exit' to exit ")
+if leave == "exit":
+    exit()
